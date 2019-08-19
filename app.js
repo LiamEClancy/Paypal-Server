455349bc4574118cdac44950640a177f500770f9
Liam Clancy <liameclancy@gmail.com>
2019-08-19 14:40:03 -0400

    Full PayPal integration.

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+'use strict';
+
 // Configure environment variables.
 require('dotenv').config();
 
 // Imports.
 const express = require('express');
 const bodyParser = require('body-parser');
+const paypalCheckout = require('@paypal/checkout-server-sdk');
+
+// Middleware for enabling async routes with Express.
+const asyncMiddleware = fn => (req, res, next) => {
+	Promise.resolve(fn(req, res, next))
+  .catch(next);
+};
 
 // Application setup.
 const app = express();
@@ -18,6 +27,9 @@ app.use(bodyParser.urlencoded({
 const APPLICATION = process.env.APPLICATION;
 const PORT = process.env.PORT;
 
+// Field variables.
+let client;
+
 // Redirect visitors to the dashboard.
 app.get('/', function (req, res) {
 	res.render('dashboard', {
@@ -25,7 +37,56 @@ app.get('/', function (req, res) {
 	});
 });
 
+app.post('/create-transaction', asyncMiddleware(async (req, res, next) => {
+	const request = new paypalCheckout.orders.OrdersCreateRequest();
+	request.prefer('return=representation');
+	request.requestBody({
+		intent: 'CAPTURE',
+		purchase_units: [{
+			amount: {
+				currency_code: 'USD',
+				value: '22.00'
+			}
+		}]
+	});
+
+	let order;
+	try {
+		order = await client.execute(request);
+	} catch (err) {
+		// Handle any errors from the call
+		console.error(err);
+		return res.send(500);
+	}
+
+	// Return a successful response to the client with the order ID
+	res.status(200).json({
+		orderID: order.result.id
+	});
+}));
+
+app.post('/approve-transaction', asyncMiddleware(async (req, res, next) => {
+	const orderID = req.body.orderID;
+
+	const request = new paypalCheckout.orders.OrdersCaptureRequest(orderID);
+	request.requestBody({});
+
+	try {
+		const capture = await client.execute(request);
+		console.log(capture);
+		res.sendStatus(200);
+	}	catch (error) {
+		console.error(error);
+		res.sendStatus(500);
+	}
+}));
+
 // Launch the application and begin the server listening.
 app.listen(PORT, function () {
 	console.log(APPLICATION, 'listening on port', PORT);
+
+	let clientID = process.env.PAYPAL_CLIENT_ID;
+	let clientSecret = process.env.PAYPAL_CLIENT_SECRET;
+	let environment = new paypalCheckout.core.SandboxEnvironment(clientID, clientSecret);
+	client = new paypalCheckout.core.PayPalHttpClient(environment);
 });
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,24 @@
 // A function which asynchronously sets up the page.
 let setup = async function (config) {
+	paypal.Buttons({
+
+		// Set up the transaction
+		createOrder: async function () {
+			let data = await $.post('/create-transaction');
+			return data.orderID;
+		},
+
+		// Capture the funds from the transaction
+		onApprove: async function (data) {
+			let status = await $.post('/approve-transaction', data);
+			console.log(status);
+			if (status === 'OK') {
+				console.log('Transaction completed successfully.');
+			} else {
+				console.log('Transaction failed.');
+			}
+		}
+	}).render('#paypal-button-container');
 };
 
 // Parse the client-side configuration file and pass to setup.
